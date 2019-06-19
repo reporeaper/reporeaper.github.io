f08ba706d1d2331e9887ed611c4f452d8fa7eb31
reporeaper <49642394+reporeaper@users.noreply.github.com>
2019-06-19 17:51:18 +0300

    добавлена кнопка "наверх"

--- CabHub/js/anchor.js
+++ CabHub/js/anchor.js
@@ -8,3 +8,17 @@
       return false;
     });
   });
+
+  //Кнопка "Наверх"
+  $(function() {
+    $(window).scroll(function() {
+        if($(this).scrollTop() != 0) {
+            $('#toTop').fadeIn();
+        } else {
+            $('#toTop').fadeOut();
+        }
+    });
+    $('#toTop').click(function() {
+        $('body,html').animate({scrollTop:0},777);
+    });
+});
