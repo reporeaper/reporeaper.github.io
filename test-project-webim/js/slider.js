e2a0af9b18f125ddcb348486fc14bcd0b89a7021
reporeaper <aupontrouge2018@gmail.com>
2019-06-29 23:37:55 +0300

    changes

---
+++ test-project-webim/js/slider.js
@@ -0,0 +1,10 @@
+$('.carousel').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    prevArrow: '<button type = "button" class = "slick-prev"><img src="images/arrow_prev.svg" width="20px" height="20px"></button>',
+    nextArrow: '<button type = "button" class = "slick-prev"><i class="fa fa-angle-right" aria-hidden="true"></i></button>',
+  });
+
+  //<img src="images/arrow_next.svg" width="20px" height="20px">
