af75051721ed2a0cc8659e41c83bda6390b618cb
reporeaper <49642394+reporeaper@users.noreply.github.com>
2019-07-01 18:09:06 +0300

    адаптив

--- test-project-webim/js/slider.js
+++ test-project-webim/js/slider.js
@@ -3,8 +3,31 @@ $('.carousel').slick({
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 3000,
-    prevArrow: '<button type = "button" class = "slick-prev"><img src="images/arrow_prev.svg" width="20px" height="20px"></button>',
-    nextArrow: '<button type = "button" class = "slick-prev"><i class="fa fa-angle-right" aria-hidden="true"></i></button>',
+    infinite: true,
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            dots: true
+          }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+      }
+    ]
   });
 
   //<img src="images/arrow_next.svg" width="20px" height="20px">
