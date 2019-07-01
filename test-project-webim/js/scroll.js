94b9d2182efbc3017ceb7a9bac6cc1314776cb9d
reporeaper <49642394+reporeaper@users.noreply.github.com>
2019-07-01 13:26:30 +0300

    fixed header added

--- test-project-webim/js/scroll.js
+++ test-project-webim/js/scroll.js
@@ -1,7 +1,20 @@
+//Smooth scroll to anchor link
 function slowScroll(id) { 
     var offset = 0;
     $('html, body').animate({ 
          scrollTop: $(id).offset().top - offset 
     }, 1000);
     return false; 
-} 
+}
+
+//Fixed header
+jQuery(function($) {
+    $(window).scroll(function(){
+        if($(this).scrollTop()>120){
+            $('.main-header').addClass('fixed'); 
+        }
+        else if ($(this).scrollTop()<120){
+            $('.main-header').removeClass('fixed');
+        }
+    });
+});
