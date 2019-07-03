536c42f665de6ef32b51cd9b71d433c6676f1c1f
reporeaper <49642394+reporeaper@users.noreply.github.com>
2019-07-03 12:16:23 +0300

    Regul Test Project

---
+++ regul-group-test-project/js/anchor.js
@@ -0,0 +1,24 @@
+﻿$(document).ready(function () {
+    $("a[href*=#]").on("click", function (e) {
+      var anchor = $(this);
+      $('html, body').stop().animate({
+        scrollTop: $(anchor.attr('href')).offset().top
+      }, 777);
+      e.preventDefault();
+      return false;
+    });
+  });
+
+  //Кнопка "Наверх"
+  $(function() {
+    $(window).scroll(function() {
+        if($(this).scrollTop() != 0) {
+            $('#toTop').fadeIn();
+        } else {
+            $('#toTop').fadeOut();
+        }
+    });
+    $('#toTop').click(function() {
+        $('body,html').animate({scrollTop:0},777);
+    });
+});
---
+++ regul-group-test-project/js/slider.js
@@ -0,0 +1,31 @@
+$('.carousel').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    infinite: true,
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1
+          }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1
+          }
+      },
+      {
+        breakpoint: 320,
+        settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+      }
+    ]
+  });
