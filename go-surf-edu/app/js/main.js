867169cefd02ea8221e8c0a03d850fd8dfd21c9e
reporeaper <49642394+reporeaper@users.noreply.github.com>
2020-03-31 20:16:31 +0300

    changes

--- go-surf-edu/app/js/main.js
+++ go-surf-edu/app/js/main.js
@@ -30,13 +30,53 @@ $(function () {
         focusOnSelect: true
     })
 
-    $('.travel__slider').slick({
+    $('.holder__slider').slick({
         slidesToShow: 1,
         slidesToScroll: 1,
         fade: true,
         prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/arrows-left.svg" alt=""></img>',
         nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/arrows-right.svg" alt=""></img>'
     })
+
+    $('<div class="quantity-nav"><div class="quantity-button quantity-up"><img src="img/plus-btn.svg" alt=""></img></div><div class="quantity-button quantity-down"><img src="img/minus-btn.svg" alt=""></img></div></div>').insertAfter('.quantity input');
+    $('.quantity').each(function() {
+      var spinner = $(this),
+        input = spinner.find('input[type="number"]'),
+        btnUp = spinner.find('.quantity-up'),
+        btnDown = spinner.find('.quantity-down'),
+        min = input.attr('min'),
+        max = input.attr('max');
+
+      btnUp.click(function() {
+        var oldValue = parseFloat(input.val());
+        if (oldValue >= max) {
+          var newVal = oldValue;
+        } else {
+          var newVal = oldValue + 1;
+        }
+        spinner.find("input").val(newVal);
+        spinner.find("input").trigger("change");
+      });
+
+      btnDown.click(function() {
+        var oldValue = parseFloat(input.val());
+        if (oldValue <= min) {
+          var newVal = oldValue;
+        } else {
+          var newVal = oldValue - 1;
+        }
+        spinner.find("input").val(newVal);
+        spinner.find("input").trigger("change");
+      });
+
+    });
+
+    $('.quantity-button').on('click', function(){
+        let summ = $('.nights').val() * $('.summ').data('nights') + ($('.guests').val() - 1) * $('.summ').data('guests')
+        $('.summ').html('$' + summ)
+    })
+
+    
 
 
 });
