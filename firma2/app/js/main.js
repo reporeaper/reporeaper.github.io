a604887c5d2e42d966aeb56037d37497c997ad2c
reporeaper <49642394+reporeaper@users.noreply.github.com>
2020-05-30 00:10:48 +0300

    layout

---
+++ firma2/app/js/main.js
@@ -0,0 +1,12 @@
+$(function () {
+
+    $('.partners__slider').slick({
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        arrows: false,
+        dots: false,
+        infinite: true,
+        autoplay: true
+    })
+
+})
