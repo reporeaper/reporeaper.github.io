a22f3ae691abbfe2957c9ccbade1bea0e92a772b
reporeaper <49642394+reporeaper@users.noreply.github.com>
2020-06-01 22:12:27 +0300

    in work

--- firma2/app/js/main.js
+++ firma2/app/js/main.js
@@ -1,12 +1,17 @@
 $(function () {
 
-    $('.partners__slider').slick({
-        slidesToShow: 6,
-        slidesToScroll: 1,
-        arrows: false,
-        dots: false,
-        infinite: true,
-        autoplay: true
-    })
+  $('.partners__slider').slick({
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    arrows: false,
+    dots: false,
+    infinite: true,
+    autoplay: true
+  })
+
+  $('.help__menu').on('click', function () {
+    $('.help__menu-dropdown').toggleClass('visually-hidden'),
+    $('.rect').toggleClass('visually-hidden')
+  })
 
 })
