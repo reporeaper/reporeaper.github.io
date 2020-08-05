a5a7185c6e64059d477b8c9f43a29c06d2687c0a
reporeaper <49642394+reporeaper@users.noreply.github.com>
2020-08-05 16:25:09 +0300

    done

--- firma2/app/js/main.js
+++ firma2/app/js/main.js
@@ -6,12 +6,45 @@ $(function () {
     arrows: false,
     dots: false,
     infinite: true,
-    autoplay: true
+    autoplay: true,
+    responsive: [
+      {
+        breakpoint: 990,
+        settings: {
+          slidesToShow: 5,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 790,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1
+        }
+      }
+    ]
   })
 
   $('.help__menu').on('click', function () {
     $('.help__menu-dropdown').toggleClass('visually-hidden'),
       $('.rect').toggleClass('visually-hidden')
   })
+
+  $('.accordion-item__title').click(function () {
+    const parent = $(this).parent()
+
+    if (parent.hasClass('accordion-item--active')) {
+      parent.removeClass('accordion-item--active')
+    } else {
+      $('.accordion-item').removeClass('accordion-item--active')
+      parent.addClass('accordion-item--active')
+    }
+  })
+
+  // $(document).ready(function () {
+  //   $('.accordion-item__title').click(function () {
+  //     $(this).next('.accordion-item__content').slideToggle(200)
+  //   })
+  // })
 
 })
